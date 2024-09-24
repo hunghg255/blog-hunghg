04b9b4c4e3ac2bf0b633be30c831a28bc1bd5578
hunghg255 <giahunghust@gmail.com>
2024-09-24 15:07:49 +0700

    chore: update giscues

--- src/components/Comment/Comment.tsx
+++ src/components/Comment/Comment.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import Giscus from '@giscus/react';
+import { useRouter } from 'next/router';
 
 const Comment = () => {
+  const router = useRouter();
+  console.log(router.asPath);
+
   return (
     <div className={'containerComment'}>
       <Giscus
+        key={router.asPath}
         id='comments'
         repo='hunghg255/blog-hunghg'
         repoId='R_kgDOKR9S5w'
